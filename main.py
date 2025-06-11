8cd2a126b2962e9b180b8a3b4982f8f675ca5028
Lilithbtw <lili.macias@proton.me>
2025-06-11 20:18:01 +0200

    Improved File Structure

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder='templates')
 port = 5000
 
 
-@app.route("/", methods=['GET','POST'])
+@app.route("/", methods=['GET'])
 def index():
     return render_template('index.html')
 
